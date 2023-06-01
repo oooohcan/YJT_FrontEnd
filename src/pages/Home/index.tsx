cd9b5dc6e18bdbae3c96dd03c61cbbc50bd49ce9
oooohcan <1489332064@qq.com>
2023-06-01 13:55:17 +0800

    头像下拉ui完成

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel, history } from '@umijs/max';
-import { Button } from 'antd';
 
 import styles from './index.less';
 import Guide from './Guide';
@@ -15,17 +14,8 @@ const HomePage: React.FC = () => {
       <div className={styles.container}>
         <Guide name={trim(name)} />
       </div>
-      <Button
-        style={{ position: 'absolute', right: 10, top: 10 }}
-        onClick={() => {
-          history.push('/user/login');
-        }}
-      >
-        点我登录
-      </Button>
       <br />
       <br />
-
       <ProblemList />
     </PageContainer>
   );
