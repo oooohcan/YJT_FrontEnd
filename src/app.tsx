94fb759ec3b4d6fd9b304cc58ad9a97cd65beb67
oooohcan <1489332064@qq.com>
2023-06-02 22:20:46 +0800

    增加注释

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
 import RightContent from './components/RightContent';
-
+// umimax运行时配置
+// 初始化全局状态
 export async function getInitialState() {
   return { init: 'success' };
 }
-
+// 运行时布局配置
 export const layout = () => {
   return {
     layout: 'top',
