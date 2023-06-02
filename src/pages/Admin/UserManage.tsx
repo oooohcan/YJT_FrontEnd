f5eae99a331de1551023a84fe5b81fb9b68c7047
oooohcan <1489332064@qq.com>
2023-06-02 19:52:06 +0800

    增加用户管理页

--- src/pages/Admin/UserManage.tsx
+++ src/pages/Admin/UserManage.tsx
@@ -1,28 +1,52 @@
-import type {
-  ActionType,
-  ProColumns,
-  ProTable,
-  TableDropdown,
-} from '@ant-design/pro-components';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
+import { ProTable, TableDropdown } from '@ant-design/pro-components';
+import { useRef } from 'react';
 import { searchUser } from '@/services/Api/UserController';
 import { Image } from 'antd';
-import { useRef } from 'react';
+
 const columns: ProColumns<API.CurrentUser>[] = [
-  { dataIndex: 'id', valueType: 'indexBorder', width: 48 },
-  { title: '用户名', dataIndex: 'username', copyable: true },
-  { title: '账号', dataIndex: 'userAccount', copyable: true },
+  {
+    dataIndex: 'id',
+    valueType: 'indexBorder',
+    width: 48,
+  },
+  {
+    title: '用户名',
+    dataIndex: 'username',
+    copyable: true,
+  },
+  {
+    title: '账号',
+    dataIndex: 'userAccount',
+    copyable: true,
+  },
   {
     title: '头像',
     dataIndex: 'avatarUrl',
     render: (_, record) => (
       <div>
-        <Image src={record.avatarurl} width={30} height={30} />
+        <Image src={record.avatarUrl} width={30} height={30} />
       </div>
     ),
   },
-  { title: '性别', dataIndex: 'gender' },
-  { title: '邮箱', dataIndex: 'email', copyable: true },
-  { title: '状态', dataIndex: 'userStatus' },
+  {
+    title: '性别',
+    dataIndex: 'gender',
+  },
+  {
+    title: '电话',
+    dataIndex: 'phone',
+    copyable: true,
+  },
+  {
+    title: '邮箱',
+    dataIndex: 'email',
+    copyable: true,
+  },
+  {
+    title: '状态',
+    dataIndex: 'userStatus',
+  },
   {
     title: '角色',
     dataIndex: 'userRole',
@@ -32,8 +56,15 @@ const columns: ProColumns<API.CurrentUser>[] = [
       1: { text: '管理员', status: 'Success' },
     },
   },
-  { title: '邀请码', dataIndex: 'inviteCode' },
-  { title: '创建时间', dataIndex: 'createTime', valueType: 'dateTime' },
+  {
+    title: '邀请码',
+    dataIndex: 'inviteCode',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    valueType: 'dateTime',
+  },
   {
     title: '操作',
     valueType: 'option',
@@ -51,7 +82,7 @@ const columns: ProColumns<API.CurrentUser>[] = [
         查看
       </a>,
       <TableDropdown
-        key={'actionGroup'}
+        key="actionGroup"
         onSelect={() => action?.reload()}
         menus={[
           { key: 'copy', name: '复制' },
@@ -72,31 +103,45 @@ export default () => {
       request={async (params = {}, sort, filter) => {
         console.log(sort, filter);
         const userList = await searchUser();
-        return { data: userList };
+        return {
+          data: userList,
+        };
       }}
-      editable={{ type: 'multiple' }}
+      editable={{
+        type: 'multiple',
+      }}
       columnsState={{
         persistenceKey: 'pro-table-singe-demos',
         persistenceType: 'localStorage',
         onChange(value) {
-          console.log('value:', value);
+          console.log('value: ', value);
         },
       }}
-      rowKey={'id'}
-      search={{ labelWidth: 'auto' }}
-      options={{ setting: { listsHeight: 400 } }}
+      rowKey="id"
+      search={{
+        labelWidth: 'auto',
+      }}
+      options={{
+        setting: {
+          listsHeight: 400,
+        },
+      }}
       form={{
+        // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
         syncToUrl: (values, type) => {
           if (type === 'get') {
             return {
               ...values,
-              created_at: [values.starTime, values.endTime],
+              created_at: [values.startTime, values.endTime],
             };
           }
           return values;
         },
       }}
-      pagination={{ pageSize: 5, onChange: (page) => console.log(page) }}
+      pagination={{
+        pageSize: 5,
+        onChange: (page) => console.log(page),
+      }}
       dateFormatter="string"
       headerTitle="高级表格"
     />
